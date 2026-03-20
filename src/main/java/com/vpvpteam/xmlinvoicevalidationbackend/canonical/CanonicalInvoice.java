f47bce9ee3c20c4ed4897d349e52141817ea4b3c
Vladyslav Petrenko <vladyslav533p@icloud.com>
2026-03-20 20:29:36 +0100

    Add InvoiceOptionalFields and optional section to CanonicalInvoice

--- src/main/java/com/vpvpteam/xmlinvoicevalidationbackend/canonical/CanonicalInvoice.java
+++ src/main/java/com/vpvpteam/xmlinvoicevalidationbackend/canonical/CanonicalInvoice.java
@@ -1,8 +1,13 @@
 package com.vpvpteam.xmlinvoicevalidationbackend.canonical;
+
 import java.util.List;
 
 public class CanonicalInvoice {
+    // Mandatory fields
     private InvoiceHeader header;
     private List<InvoiceLine> lines;
     private InvoiceTotals totals;
+
+    // Optional fields
+    private InvoiceOptionalFields optionalFields;
 }
